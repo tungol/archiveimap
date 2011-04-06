dcb5d132ca4e17dea42d0ec8ee311f748b8b557b
tungolcraft <tungolcraeft@gmail.com>
2011-04-06 03:56:05 -0400

    get metadata into __init__.py so it's available at the package level, docstring edits

---
+++ archiveimap/__init__.py
@@ -0,0 +1,9 @@
+import archiveimap
+
+__doc__ = archiveimap.__doc__
+__version__ = archiveimap.__version__
+__author__ = archiveimap.__author__
+__author_email__ = archiveimap.__author_email__
+__description__ = archiveimap.__description__
+__license__ = archiveimap.__license__
+__url__ = archiveimap.__url__
--- archiveimap/archiveimap.py
+++ archiveimap/archiveimap.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python
 
-'''
-Archiveimap is a tool to keep an email account in version control. It's a
-pretty simple wrapper around offlineimap and git. The commit message is what
+'''\
+ArchiveIMAP is a tool to keep an email account in version control. It's a
+pretty simple wrapper around OfflineIMAP and git. The commit message is what
 offlineimap prints to stdout while running.
 
 To use:
-Make sure offlineimap and git are both installed and that offlineIMAP is
-properly configured. If you want to archive all accounts in offlineIMAP,
+Make sure offlineimap and git are both installed and that OfflineIMAP is
+properly configured. If you want to archive all accounts in OfflineIMAP,
 using config files in the default locations and the system git author,
 this is all that is needed. For other configurations, options can be set in
 a configuration file or at the command line. The command line takes
